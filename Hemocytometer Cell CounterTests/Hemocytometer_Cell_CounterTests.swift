ec59ca38b569f01139c350f590dca584c75f6f99
Bryan Zhang <bzhang57@gatech.edu>
2016-08-01 18:06:27 -0400

    Initial Commit

---
+++ Hemocytometer Cell CounterTests/Hemocytometer_Cell_CounterTests.swift
@@ -0,0 +1,36 @@
+//
+//  Hemocytometer_Cell_CounterTests.swift
+//  Hemocytometer Cell CounterTests
+//
+//  Created by Bryan Zhang on 8/1/16.
+//  Copyright © 2016 Bryan Zhang. All rights reserved.
+//
+
+import XCTest
+@testable import Hemocytometer_Cell_Counter
+
+class Hemocytometer_Cell_CounterTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
